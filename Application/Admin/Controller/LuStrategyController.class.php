c489726874be8c3509e904263d3a76215f38cde7
xmxkkk <908581016@qq.com>
2016-12-02 19:46:58 +0800

    aa

--- Application/Admin/Controller/LuStrategyController.class.php
+++ Application/Admin/Controller/LuStrategyController.class.php
@@ -98,74 +98,17 @@ class LuStrategyController extends AdminController {
     }
 
     public function edit($id=0){
+        $data=M('LuStrategy')->where(array('id' => $id))->find();
+        $this->combine($data);
+        $this->assign('data',$data);
 
-        if(IS_POST){
-            $this->update();
-        }else{
-            $data=M('LuStrategy')->where(array('id' => $id))->find();
-            $this->combine($data);
-            $this->assign('data',$data);
+        $this->assign('aciton','edit');
 
-            $this->display();
-        }
+        $this->display();
     }
     public function add(){
-        if(IS_POST){
-            $filters=trim(I('post.filters'));
-            $title=trim(I('post.title'));
-            $attr=trim(I('post.attr'));
-            $img=trim(I('post.img'));
-            $type=trim(I('post.type'));
-            $status=trim(I('post.status'));
-
-            if(empty($title)){
-                $this->error('名字不能为空');
-            }
-            if(empty($attr)){
-                $this->error('描述不能为空');
-            }
-            if(empty($img)){
-                $this->error('图片不能为空');
-            }
-
-            $arr=json_decode($filters);
-            if(count($arr)<1){
-                $this->error("过滤器不足");
-            }
-
-            $LuStrategy = D('LuStrategy');
-            $data=array(
-                'title'=>$title,
-                'attr'=>$attr,
-                'img'=>$img,
-                'type'=>$type,
-                'status'=>$status
-                );
-
-            $id=$LuStrategy->data($data)->add();
-
-            M('LuStrategyFilter')->where(array('id'=>$id))->delete();
-
-            for($i=0;$i<count($arr);$i++){
-                $filter=$arr[$i]->filter;
-                $condition=$arr[$i]->condition;
-
-                M('LuStrategyFilter')->data(array(
-                    'id'        =>$id,
-                    'filter'    =>$filter,
-                    'condition' =>$condition
-                    ))->add();
-            }
-
-            if($id){
-                   $this->success('新增成功', U('index'));
-            } else {
-                $this->error('新增失败');
-            }
-        }else{
-             $this->display("edit");
-        }
-      
+        $this->assign('aciton','add');
+        $this->display("edit");
     }
 
     public function update(){
@@ -177,9 +120,32 @@ class LuStrategyController extends AdminController {
         $type=I('post.type');
         $status=I('post.status');
         
+        if(empty($title)){
+            $this->error('名字不能为空');
+        }
+        if(empty($attr)){
+            $this->error('描述不能为空');
+        }
+        if(empty($img)){
+            $this->error('图片不能为空');
+        }
+
         $arr=json_decode($filters);
         if(count($arr)<1){
             $this->error("过滤器不足");
+        }
+        
+        $data=array(
+            'title'=>$title,
+            'attr'=>$attr,
+            'img'=>$img,
+            'type'=>$type,
+            'status'=>$status
+            );
+        if(empty($id)){
+            $id=M('LuStrategy')->data($data)->add();
+        }else{
+            M('LuStrategy')->where(array('id'=>$id))->data($data)->save();
         }
 
         M('LuStrategyFilter')->where(array('id'=>$id))->delete();
@@ -195,16 +161,14 @@ class LuStrategyController extends AdminController {
                 ))->add();
         }
 
-        $data=array(
-            'title'=>$title,
-            'attr'=>$attr,
-            'img'=>$img,
-            'type'=>$type,
-            'status'=>$status
-            );
-        M('LuStrategy')->where(array('id'=>$id))->data($data)->save();
-
-        $this->success("更新成功");
+        $result=array();
+        $result['info']   =   "更新成功";
+        $result['status'] =   1;
+        if(I('post.action')=='add'){
+            $result['url']    =   U('index');
+        }
+        $result['id']     =   $id;
+        $this->ajaxReturn($result);
     }
     public function run_filter(){
         $result=new \stdClass();
@@ -215,17 +179,15 @@ class LuStrategyController extends AdminController {
         if($id==""){
             $id=-1;
             $filters=I('post.filters');
-            
+            $filters=json_decode($filters);
         }else{
             $filters=M("LuStrategyFilter")->where(array("id"=>$id))->select();
-
         }
-        if(count(json_decode($filters))==0){
-            $result->status=1;
-            $this->ajaxReturn($result);
+        if(count($filters)==0){
+            $this->error("过滤器不足");
         }
 
-        $filters=json_encode(json_decode($filters));
+        $filters=json_encode($filters);
 
         $md5=md5(rand(0,2000000000).":".rand(0,2000000000).":".rand(0,2000000000));
 
@@ -248,10 +210,17 @@ class LuStrategyController extends AdminController {
         $changeRate=M("LuStrategyChangeRate")->where(array("id"=>$id))->find();
 
         
-        $result->status=0;
-        $result->md5=$md5;
-        $result->stocks=$stocks;
-        $result->changeRate=$changeRate;
+        $result=array();
+
+        $result['status']=1;
+        $result['info']="运行成功";
+        if(I('post.action')=='add'){
+            $result['url']    =   U('index');
+        }
+        
+        $result['md5']=$md5;
+        $result['stocks']=$stocks;
+        $result['changeRate']=$changeRate;
 
         $this->ajaxReturn($result);
     }
@@ -267,5 +236,16 @@ class LuStrategyController extends AdminController {
         M("LuStrategyStock")->where(array("id"=>$id))->delete();
         
         $this->success('删除成功');
+    }
+    public function hangye(){
+        $level1=M("StockCompanyHangye")->group('level1')->select();
+        $level2=M("StockCompanyHangye")->group('level2')->select();
+        $level3=M("StockCompanyHangye")->group('level3')->select();
+        
+        $this->assign('level1',$level1);
+        $this->assign('level2',$level2);
+        $this->assign('level3',$level3);//print_r($level3);
+        
+        $this->display();
     }
 }
